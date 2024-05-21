cccad6557dbb8d73b1549c8bbb2643009260eeb1
Rushabh Dabhade <rushabhdabhade30@gmail.com>
2024-05-21 13:21:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import SearchBar from './SearchBar'
+import React from 'react';
+import SearchBar from './SearchBar';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-
-      <nav class=" sticky top-0 z-50 bg-gray-200 border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to='' class="flex items-center">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052" class="h-8 mr-3" alt="YouTube" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">YouTube</span>
+      {/* Navbar with sticky positioning, z-index for layering, and dark mode support */}
+      <nav className="sticky top-0 z-50 bg-gray-200 border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          {/* Logo and branding */}
+          <Link to="/" className="flex items-center">
+            <img 
+              src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052" 
+              className="h-8 mr-3" 
+              alt="YouTube" 
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">YouTube</span>
           </Link>
-
+          
+          {/* Search bar component */}
           <SearchBar />
         </div>
       </nav>
-
-
-
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
